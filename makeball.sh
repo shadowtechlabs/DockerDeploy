8c507463778fb800e321a018df33d3efda6f6dfe
static74 <shane.davis74@gmail.com>
2025-02-14 08:44:34 -0600

    Delete makeball.sh

--- makeball.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# check to see if the tar exists - if not, make a new one.
-if [ ! -f shok.tar ]; then
-    tar --exclude={"installer.sh","makeball.sh","shok.tar"} -cf shok.tar .
-else
-    tar --exclude={"installer.sh","makeball.sh","shok.tar"} -uf shok.tar .
-fi
