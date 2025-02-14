7b1c794a767051f95b7939130281e4d17dd1139f
static74 <shane.davis74@gmail.com>
2025-02-14 08:43:58 -0600

    Makeball

--- makeball.sh
+++ makeball.sh
@@ -1,2 +1,8 @@
-!#/bin/bash
-tar --exclude=installer.sh --makeball.sh -uf shok.tar.gz .
+#!/bin/bash
+
+# check to see if the tar exists - if not, make a new one.
+if [ ! -f shok.tar ]; then
+    tar --exclude={"installer.sh","makeball.sh","shok.tar"} -cf shok.tar .
+else
+    tar --exclude={"installer.sh","makeball.sh","shok.tar"} -uf shok.tar .
+fi
